bcf57e5030c3568395b0c6fd25cf6008d316e5a3
Amy <chiangyenhui@gmail.com>
2025-01-10 12:07:26 -0800

    Update vercel.json

--- server/api/index.js
+++ server/api/index.js
@@ -59,6 +59,8 @@ app.get("/chat", async (req, res) => {
   }
 });
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
